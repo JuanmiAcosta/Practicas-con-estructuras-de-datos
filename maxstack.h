4e6bd9f32765f8b95972f8b9d974304ee90495c7
Juan Miguel Acosta Ortega <100489090+JuanmiAcosta@users.noreply.github.com>
2022-10-22 13:25:17 +0200

    Add files via upload

--- maxstack.h
+++ maxstack.h
@@ -10,21 +10,23 @@
 
 using namespace std;
 
-/*
+
 struct element {
     int value; // Current value to store
     int maximum; // Current max value in the structure
-}^*/
+};
+
+ostream& operator<<(ostream &flujo, const element &elem);
 
 class MaxStack{
 private:
-    queue<int> cola;
-    void InvertQueue(queue<int>& cola); // Función auxiliar que invierte la cola (Utilidad: en funcion pop)
-public:
+    queue<element> cola;
+    void InvertQueue(queue<element>& cola); // Función auxiliar que invierte la cola (Utilidad: en funcion pop) --> invierte la cola para poder sacar el elemento "back"
 
-    int top();
+public:
+    element top();
     bool empty() const;
-    void push(int num);
+    void push(int value);
     void pop();
     int size() const;
 
