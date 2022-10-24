918478765f9a7195368d6d39608c0d8b795ef803
Juan Miguel Acosta Ortega <100489090+JuanmiAcosta@users.noreply.github.com>
2022-10-24 11:20:31 +0200

    Add files via upload

--- maxstack.h
+++ maxstack.h
@@ -23,10 +23,37 @@ private:
     void InvertQueue(queue<element>& cola); // Función auxiliar que invierte la cola (Utilidad: en funcion pop) --> invierte la cola para poder sacar el elemento "back"
 
 public:
+    /**
+      @brief Devuelve el elemento del top de la pila (back de la cola interna), y este se puede modificar.
+      @pre Si pila.size() == 0 no devuelve nada.
+    **/
     element top();
+
+    /**
+      @brief Devuelve un valor booleano que indica si la pila está vacía o no (Averigua si la cola interna está vacía).
+      @pre Ha de llamrarlo una pila (MaxStack).
+    **/
     bool empty() const;
+
+    /**
+      @brief Reserva un espacio más de memoria para insertar otro elemento en la pila y lo coloca en ese espacio, si el valor es menor que el máximo anterior el máximo insertado se actualizará al anterior.
+      @param value El valor es el "value", hará cálculos internos para averiguar cuál será el "maximum" adecuado.
+      @pre El valor ha de ser un entero
+      @post El "size()" de la pila aumenta en uno.
+    **/
     void push(int value);
+
+    /**
+      @brief Quita el elemento del top de la pila, también se encarga de hacer un "resize()" (Internamente invierte la cola en una pila auxiliar, quita el elemento de su top, y vuelve a invertirla).
+      @pre Si está vacía la pila no hará nada
+      @post El "size()" de la pila decrementará en uno.
+    **/
     void pop();
+
+    /**
+      @brief Para calcular el "size" de la pila, calula el size de la cola interna.
+      @post Devuelve un valor entero como tamaño.
+    **/
     int size() const;
 
 };
